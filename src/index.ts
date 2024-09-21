f4316ef0281c3dac6439f030051dbbda068bbfa0
Micah Cowan <micah@addictivecode.org>
2024-09-21 10:40:49 -0700

    Expose LS as global.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import $ from 'jquery';
+import LS from './lstor';
 import * as YT from './youtube';
 import { App } from './app';
 
+(window as any).LS = LS;
 let outer = $('<div id="outer"/>').appendTo($('body'));
 let main = $('<div id="main"/>').appendTo(outer);
 let app = new App(main, new YT.Api);
